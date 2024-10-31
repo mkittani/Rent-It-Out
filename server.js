e251509ecf1c342aa5ddcc164088e6d0a70b5e17
Mohamad Kittani <97052577+mkittani@users.noreply.github.com>
2024-10-31 22:55:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const userRoutes = require('./routes/userRouter');
 const itemRoutes = require('./routes/itemRouter');
 const transactionRoutes = require('./routes/transactionRouter');
 const securityRoutes = require('./routes/securityRouter'); // Add security deposit routes
+const reviewRoutes = require('./routes/reviewRouter');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -25,6 +26,8 @@ app.use('/api/users', userRoutes);
 app.use('/api/items', itemRoutes);
 app.use('/api/transactions', transactionRoutes);
 app.use('/api/security-deposits', securityRouter);
+app.use('/api/reviews', reviewRoutes);
+
 app.get('/', (req, res) => {
   res.json({ message: 'All good!' });
 });
