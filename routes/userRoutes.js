6a8b72687c51b1cc7fa96c2c95c2acb464b3267e
Qais Hweidi <72994769+Qais-Hweidi@users.noreply.github.com>
2024-10-31 18:13:45 +0200

    Delete routes/userRoutes.js

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-router.post('/verify', userController.submitVerification);
-router.get('/verify/status/:userId', userController.checkVerificationStatus);
-
-module.exports = router;
